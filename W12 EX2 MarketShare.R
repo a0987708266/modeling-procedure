78e81b0a363f46f8bd049cb27ae1cbec7ad87a82
JYP\a0987 <a0987708266@gmail.com>
2023-12-28 21:42:31 +0800

    Phase I: Identify the problem

--- W12 EX2 MarketShare.R
+++ W12 EX2 MarketShare.R
@@ -2,7 +2,7 @@
 #建立產品市場份額(Market Share)與其他影響因子間的迴歸關係,
 #以了解其他影響因子與產品市場份額(Market Share)間的關係,
 #並做為後續預測產品市場份額之用,
-#以提供廠商參考
+#以提供廠商參考用
 
 #Phase II: MAKE ASSUMPTIONS AND DEFINE ESSENTIAL VARIABLES
 #Four basic assumptions
